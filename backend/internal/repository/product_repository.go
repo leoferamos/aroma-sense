1ac731eed4116ee8eecde17abfc7589ca56cd15d
leoferamos <fernandesramosleonardo@gmail.com>
2025-11-28 16:03:35 -0300

    fix(import): update import path for utils in product repository

--- backend/internal/repository/product_repository.go
+++ backend/internal/repository/product_repository.go
@@ -9,7 +9,7 @@ import (
 
 	"github.com/leoferamos/aroma-sense/internal/dto"
 	"github.com/leoferamos/aroma-sense/internal/model"
-	"github.com/leoferamos/aroma-sense/utils"
+	"github.com/leoferamos/aroma-sense/internal/utils"
 	"gorm.io/gorm"
 )
 
